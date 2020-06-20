9eacd117a31b4a926d2ce4e8f9131fd9130f8b62
ksalrin <salrindesigngeek@gmail.com>
2020-06-20 03:58:54 +0000

    Group Project Prometheus install

---
+++ group_project1/security_group.tf
@@ -0,0 +1,44 @@
+resource "aws_security_group" "pro_graf_security" {
+  name = "pro_graf_security"
+  description = "Allow inbound traffic"
+
+  ingress {
+    description = "ssh_port"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+     cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "prometh_node_port"
+    from_port   = 9100
+    to_port     = 9100
+    protocol    = "tcp"
+     cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "prometh_port"
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+     cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "Grafana_port"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+     cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "pro_graf_security"
+  }
+}
+
