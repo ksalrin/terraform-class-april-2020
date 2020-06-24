d43d4524feee838424f80a49f6fbbd10adb39093
ksalrin <salrindesigngeek@gmail.com>
2020-06-24 04:41:55 +0000

    updated with centos

--- group_project1/null_resource.tf
+++ group_project1/null_resource.tf
@@ -1,38 +1,38 @@
-  resource "null_resource" "mine" {
-    triggers = {
-        always_run = "${timestamp()}"
-    }    
-   depends_on = ["aws_instance.node"]
-  provisioner   "file" {
-    connection {
-        host        = "${aws_instance.node.public_ip}"
-        type        = "ssh"
-        user        = "ec2-user"
-        private_key = "${file("~/.ssh/id_rsa")}"
-    }
-    source  =  "./configuration_files/prometheus_configs/node_exporter.service"
-    destination = "/tmp/node_exporter.service"
-  },
-  depends_on = ["aws_instance.node"]
-  provisioner   "remote-exec" {
-    connection {
-        host        = "${aws_instance.node.public_ip}"
-        type        = "ssh"
-        user        = "ec2-user"
-        private_key = "${file("~/.ssh/id_rsa")}"
-    }
-    inline = [
-      "sudo mv node_exporter-0.18.1.linux-amd64 /etc/prometheus/node_exporter",
-      # change ownership to prometheus
-      "sudo chown -R prometheus:prometheus /etc/prometheus/node_exporter",
-      # move service file to /etc folder and change ownership to root
-      "sudo mv /tmp/node_exporter.service /etc/systemd/system/node_exporter.service",
-      "sudo chown root:root /etc/systemd/system/node_exporter.service",
-      # start and enable services
-      "sudo systemctl daemon-reload", 
-      "sudo systemctl start node_exporter",
-      "sudo systemctl enable node_exporter",
-      "sudo systemctl status node_exporter"
-    ]
-  }
-}
+#   resource "null_resource" "mine" {
+#     triggers = {
+#         always_run = "${timestamp()}"
+#     }    
+#    depends_on = ["aws_instance.node"]
+#   provisioner   "file" {
+#     connection {
+#         host        = "${aws_instance.node.public_ip}"
+#         type        = "ssh"
+#         user        = "centos"
+#         private_key = "${file("~/.ssh/id_rsa")}"
+#     }
+#     source  =  "./configuration_files/prometheus_configs/node_exporter.service"
+#     destination = "/tmp/node_exporter.service"
+#   },
+#   depends_on = ["aws_instance.node"]
+#   provisioner   "remote-exec" {
+#     connection {
+#         host        = "${aws_instance.node.public_ip}"
+#         type        = "ssh"
+#         user        = "centos"
+#         private_key = "${file("~/.ssh/id_rsa")}"
+#     }
+#     inline = [
+#       "sudo mv node_exporter-0.18.1.linux-amd64 /etc/prometheus/node_exporter",
+#       # change ownership to prometheus
+#       "sudo chown -R prometheus:prometheus /etc/prometheus/node_exporter",
+#       # move service file to /etc folder and change ownership to root
+#       "sudo mv /tmp/node_exporter.service /etc/systemd/system/node_exporter.service",
+#       "sudo chown root:root /etc/systemd/system/node_exporter.service",
+#       # start and enable services
+#       "sudo systemctl daemon-reload", 
+#       "sudo systemctl start node_exporter",
+#       "sudo systemctl enable node_exporter",
+#       "sudo systemctl status node_exporter"
+#     ]
+#   }
+# }
--- group_project1/prometheus_node.tf
+++ group_project1/prometheus_node.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "node" {
   depends_on = ["aws_instance.web"]
-  ami           = "${data.aws_ami.amazon.id}"
+  ami           = "${data.aws_ami.centos7.id}"
   key_name      = "${aws_key_pair.deployer.key_name}"
   iam_instance_profile = "${aws_iam_instance_profile.prometheus_profile.name}"
   subnet_id = "${aws_subnet.public2.id}"
@@ -10,7 +10,7 @@ resource "aws_instance" "node" {
     connection {
         host        = "${self.public_ip}"
         type        = "ssh"
-        user        = "ec2-user"
+        user        = "centos"
         private_key = "${file("~/.ssh/id_rsa")}"
     }
     inline = [
@@ -25,6 +25,39 @@ resource "aws_instance" "node" {
       # Installs node exporter 
       "wget https://github.com/prometheus/node_exporter/releases/download/v0.18.1/node_exporter-0.18.1.linux-amd64.tar.gz",
       "tar xvfz node_exporter-0.18.1.linux-amd64.tar.gz",
+    ]
+  },
+   depends_on = ["aws_instance.node"]
+  provisioner   "file" {
+    connection {
+        host        = "${aws_instance.node.public_ip}"
+        type        = "ssh"
+        user        = "centos"
+        private_key = "${file("~/.ssh/id_rsa")}"
+    }
+    source  =  "./configuration_files/prometheus_configs/node_exporter.service"
+    destination = "/tmp/node_exporter.service"
+  },
+  depends_on = ["aws_instance.node"]
+  provisioner   "remote-exec" {
+    connection {
+        host        = "${aws_instance.node.public_ip}"
+        type        = "ssh"
+        user        = "centos"
+        private_key = "${file("~/.ssh/id_rsa")}"
+    }
+    inline = [
+      "sudo mv node_exporter-0.18.1.linux-amd64 /etc/prometheus/node_exporter",
+      # change ownership to prometheus
+      "sudo chown -R prometheus:prometheus /etc/prometheus/node_exporter",
+      # move service file to /etc folder and change ownership to root
+      "sudo mv /tmp/node_exporter.service /etc/systemd/system/node_exporter.service",
+      "sudo chown root:root /etc/systemd/system/node_exporter.service",
+      # start and enable services
+      "sudo systemctl daemon-reload", 
+      "sudo systemctl start node_exporter",
+      "sudo systemctl enable node_exporter",
+      "sudo systemctl status node_exporter"
     ]
   }
    tags = {
