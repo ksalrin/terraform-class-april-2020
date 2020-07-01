37d5aa6d56b8111dedc59d196eecb91041e258ed
ksalrin <salrindesigngeek@gmail.com>
2020-07-01 01:22:14 +0000

    modified class 6

--- class6/variable.tf
+++ class6/variable.tf
@@ -8,3 +8,11 @@ variable "asg_max_size" {}
 variable "tags" {
     type = map
 }
+
+
+variable "environment" {}
+variable "s3_bucket" {}
+variable "s3_folder_region" {}
+variable "s3_folder_project" {}
+variable "s3_folder_type" {}
+variable "s3_tfstate_file" {}
