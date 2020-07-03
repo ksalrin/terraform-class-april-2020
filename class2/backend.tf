0cd7db02dc689ec05eaeb13867fe3f76c5f1a10e
ksalrin <salrindesigngeek@gmail.com>
2020-07-03 00:55:37 +0000

    remote state and class 7

--- class2/backend.tf
+++ class2/backend.tf
@@ -1,7 +1,16 @@
 terraform {
-backend "s3" {
-bucket = "terraform-state-april-class-kellysalrin"
-key = "jenkins/us-east-1/tools/ohio/jenkins.tfstate"
-region = "us-east-1"
+  required_version = "0.11.14"
+  required_providers {
+    aws = {
+      version = "2.69.0"
+    }
+  }
+  
+  backend "s3" {
+    bucket = "terraform-state-april-class-kellysalrin"
+    key = "jenkins/us-east-1/tools/virginia/jenkins.tfstate"
+    region = "us-east-1"
+    #dynamodb_table = "prod"
   }
 }
+
