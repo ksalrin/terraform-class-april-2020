6e9a4be1f1c976a82e14981815ef720b408a9768
ksalrin <salrindesigngeek@gmail.com>
2020-07-08 01:31:09 +0000

    class 8 updates

--- class8/provider.tf
+++ class8/provider.tf
@@ -2,6 +2,16 @@ provider "azurerm" {
     version = "1.27" 
 } 
 
+
+# These resources are to be created before running "terraform init" 
+
 terraform { 
     required_version = "0.11.14" 
+    backend "azurerm" { 
+    resource_group_name       = "dev" 
+    storage_account_name      = "dev1kellysalrin" 
+    container_name            = "devcontainer" 
+    access_key                = "eBrtMDJ+qyeBrc7EW6Cb0FzyI5a7Y80BYDWGLH3SwcBzL6LFvYwCQpBy4ivL+qMoiIrzjZfeDSkWG9RSqiWhig==" 
+    key                       = "dev_terraform.tfstate" 
+  } 
 } 
